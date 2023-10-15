6e343064b276cb1b88f7bba7de1aa22968ba16dd
Andy-Valentin Ariciu <andyariciu@student.upg-ploiesti.ro>
2023-10-15 20:33:15 +0100

    Add project files.

---
+++ FirstIndexOccurence/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace FirstIndexOccurence
+{
+    class Program
+    {
+        public static int StrStr(string haystack, string needle)
+        {
+            if (haystack.Contains(needle))
+                return haystack.IndexOf(needle);
+            else
+                return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write the first word: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Write the needle: ");
+            string word = Console.ReadLine();
+            int index = StrStr(str, word);
+            Console.WriteLine("Index is: " + index);
+        }
+    }
+}
